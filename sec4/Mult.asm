1969f0b5e3b73c7b34dc47017640b2dd43580db8
kavos113 <kohei.hadano@gmail.com>
2025-02-16 13:32:15 +0900

    sec4 mul

---
+++ sec4/Mult.asm
@@ -0,0 +1,32 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/4/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// The algorithm is based on repetitive addition.
+
+//// Replace this comment with your code.
+@i 
+M=0
+@2
+M=0
+(LOOP)
+@i
+D=M
+@1
+D=D-M
+@END
+D;JEQ
+@0
+D=M
+@2
+M=D+M
+@i
+M=M+1
+@LOOP
+0;JMP
+(END)
+@END
+0;JMP
